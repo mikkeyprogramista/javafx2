7958b1e1182fec00ece99e43e368ed3ce0d04e16
mikkeyprogramista <mikkeyprogramista@gmail.com>
2026-01-22 15:01:08 +0100

    Add complete project

--- src/main/java/com/adz1q/javafx2/Controller.java
+++ src/main/java/com/adz1q/javafx2/Controller.java
@@ -1,14 +1,39 @@
 package com.adz1q.javafx2;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 
-public class Controller {
-    @FXML
-    private Label welcomeText;
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Hyperlink link;
+
+    @FXML
+    private Button btnClick;
+
+    @FXML
+    private Label lblInfo;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        btnClick.setOnAction(e -> lblInfo.setText("Clicked!"));
+        btnClick.setOnMouseExited(e -> lblInfo.setText("Unhovered!"));
+        link.setOnAction(e -> {
+            try {
+                Desktop.getDesktop().browse(new URI("http://youtube.com"));
+            } catch (IOException | URISyntaxException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
-module com.adz1q.javafx2 {
+module com.adz1q.javafx {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.desktop;
+    requires java.datatransfer;
 
     opens com.adz1q.javafx2 to javafx.fxml;
     exports com.adz1q.javafx2;
